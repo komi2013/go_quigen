79370ff94d68134267def194f82a963a8a5324c6
komatsu <komatsu@beenos.com>
2020-09-01 14:56:08 +0900

    change range of pointer none and date format

--- common/config.go
+++ common/config.go
@@ -1,9 +1,9 @@
 package common
 
 const GO_PORT  = ":8101"
-const CACHE_V  = "?dev20200826"
+const CACHE_V  = "?dev20200901"
 const SS_KEY   = "11111111111111111111111111111111"
 const T1_KEY   = "11111111111111111111111111111111"
 const CSRF_KEY = "11111111111111111111111111111111"
-const DB_CONNECT = "user=exam_8099 password=Zk1CGsBK dbname=shikaku sslmode=disable port=5432 host=194.135.95.163"
-const DATE_LANGUAGE = "yyyy年M月d日(WW)"
+const DB_CONNECT = "user=exam_8099 password=Zk1CGsBK dbname=kids sslmode=disable port=5432 host=194.135.95.163"
+const DATE_LANGUAGE = "yyyy MMM d(w)"
